0e96e9556352ef020df17755033a2ef20434b325
zarkasi31 <zarkasi31@gmail.com>
2020-01-31 20:23:35 +0700

    gerakan

--- tes.js
+++ tes.js
@@ -1,17 +1,48 @@
-var a, b ,c ,d;
-a=0;
-do{
-    if(a==12){
-        b=10;
-    }else if(a==13){
-        c=15;
-    }else if(a==16){
-        d=9;
-    }else{
-        d=300;
+var bg = new Image();
+bg.src = "image/gmbar.jpg";
+
+var pesawat = new Image();
+pesawat.src = "image/pesawat.png";
+
+function aksicanvas() {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+
+    canvas.width = canvas.scrollWidth;
+    canvas.height = canvas.scrollHeight;
+
+    var cW = canvas.width;
+    var cH = canvas.height;
+
+    function Bg() {
+        this.x = 0, this.y = 0;
+        this.render = function () {
+            ctx.drawImage(bg, this.x--, this.y);
+            if (this.x == -1699) {
+                this.x = 0;
+            }
+        }
     }
-    a++
-}while(a<=20);
-document.write(b=10 ? "benar <br>":"salah <br>");
-document.write(c);
-document.write(d);
+
+    function Pesawat() {
+        this.x = 100, this.y = 200, this.w = 650, this.h = 366;
+        this.render = function () {
+            ctx.drawImage(pesawat, this.x, this.y)
+        }
+        ctx
+    }
+
+    var back = new Bg();
+    var pesawat1 = new Pesawat();
+
+    function animasi() {
+        ctx.save();
+        ctx.clearRect(0, 0, cW, cH);
+
+        back.render();
+        pesawat1.render();
+        ctx.restore();
+    }
+    var interval = setInterval(animasi, 30);
+}
+window.onload = aksicanvas;
