564128522f93c865a4210a09c2cf71e5eb4e3e89
Imam_Zarkasi <44666946+Zarkasi31@users.noreply.github.com>
2020-01-31 14:47:59 +0700

    Add files via upload

---
+++ tes.js
@@ -0,0 +1,17 @@
+var a, b ,c ,d;
+a=0;
+do{
+    if(a==12){
+        b=10;
+    }else if(a==13){
+        c=15;
+    }else if(a==16){
+        d=9;
+    }else{
+        d=300;
+    }
+    a++
+}while(a<=20);
+document.write(b=10 ? "benar <br>":"salah <br>");
+document.write(c);
+document.write(d);
